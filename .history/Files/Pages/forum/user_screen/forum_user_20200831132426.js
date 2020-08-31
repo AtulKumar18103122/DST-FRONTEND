4e0ea5c15ff809553406245b34d819ba18712b72
Abhinav Marwaha <abhinavmarwaha.bt19cse@pec.edu.in>
2020-08-31 14:08:10 +0530

    forum-user-question-screens-31-08-2020

---
+++ .history/Files/Pages/forum/user_screen/forum_user_20200831132426.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('#toggler-btn').on('click', function(){  
+      $('#forum_user_work_experience_toggle').toggleClass( "fa-arrow-up-animated" );
+    });
+});
---
+++ Files/Pages/forum/user_screen/forum_user.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('#forum_user_work_experience_toggle').on('click', function(){  
+      $('#forum_user_work_experience_toggle').toggleClass( "fa-chevron-up-animated" );
+    });
+});
